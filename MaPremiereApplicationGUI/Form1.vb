d7a60d10e2b952ae1c7fe02a1d0e7440450a5de8
natha <natha@XEON>
2017-02-03 23:05:11 -0500

    Ajoutez des fichiers projet.

---
+++ MaPremiereApplicationGUI/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("HAHAHAHAHA C'EST ENCORE MOI", MsgBoxStyle.Exclamation,"Résultat")
+
+    End Sub
+End Class
